5048d5935a6006ef69b1bc192b4b68bdcff2e72d
h15 <h15@home.(none)>
2011-03-23 12:21:50 +0500

    New plugin for Joker (Data). Does not tested.

---
+++ lib/Mojolicious/Plugin/Data.pm
@@ -0,0 +1,113 @@
+use strict;
+use warnings;
+
+package Mojolicious::Plugin::Data;
+
+use base 'Mojolicious::Plugin';
+
+our $VERSION = '0.1';
+
+#
+#   Info method for Joker plugin manager.
+#
+
+sub info {
+    my ($self, $field) = @_;
+    
+    my $info = {
+        version => $VERSION,
+        author  => 'h15 <georgy.bazhukov@gmail.com>',
+        about   => 'Data bases\' interface.',
+        fields  => {},
+        depends => [],
+        config  => {
+        #
+        # Default config. Will be deleted.
+        #
+        # Should set this config in some other place...
+        #
+            driver => "Sql"
+            auth   => {
+                host    => 'dbi:mysql:code_wars',
+                user    => 'code_wars',
+                passwd  => 'SCKM4FJS57BRL49x',
+                prefix  => 'forum__',
+            }
+        }
+    };
+    
+    return $info->{$field} if $field;
+    return $info;
+}
+
+sub register {
+    my ( $self, $app ) = @_;
+    
+    my $data = Mojolicious::Plugin::Data::Data->new( $self->info('config') );
+    
+    $app->helper(
+        # For querys like $self->data->read,
+        # where read is a method of Data.
+        data => sub { $data }
+    );
+}
+
+package Mojolicious::Plugin::Data::Data;
+
+use Module::Load;
+
+#
+#   Interface for data bases.
+#
+
+sub new {
+    my ( $self, $conf ) = @_;
+    
+    my $class = "Mojolicious::Plugin::Data::" . $conf->{'driver'};
+    load $class;
+    
+    my $obj = { driver => $class->new( $conf->{'auth'} ) };
+    
+    bless $obj, $self;
+}
+
+#
+#       CRUD methods.
+#
+# Params : (table, {fields}).
+# Returns: id of new object.
+sub create {
+    return shift->{'driver'}->create(@_);
+}
+
+# Params : (table, {where}).
+# Returns: array of hashes.
+sub read {
+    return shift->{'driver'}->read(@_);
+}
+
+# Params : (table, {fields}, {where}).
+# Returns: true || false.
+sub update {
+    return shift->{'driver'}->update(@_);
+}
+
+# Params : (table, {where}).
+# Returns: true || false.
+sub delete {
+    return shift->{'driver'}->delete(@_);
+}
+
+1;
+
+__END__
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011, Georgy Bazhukov.
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=cut
+
