b0a6cbbf6bcbbec324b25e119664a144718cea2f
h15 <h15@home.(none)>
2011-04-08 22:38:27 +0600

    It does not working.

--- lib/Mojolicious/Plugin/Joker.pm
+++ lib/Mojolicious/Plugin/Joker.pm
@@ -1,6 +1,136 @@
 use strict;
 use warnings;
 
+package Mojolicious::Plugin::Joker;
+
+use base 'Mojolicious::Plugin';
+use Storable 'thaw';
+
+our $VERSION = '0.2';
+
+# Will run once.
+sub register {
+    my ( $self, $app ) = @_;
+    
+    my $session = $app->sessions;
+    
+    # Preload core plugins.
+    my @preload_modules = qw/message data user i18n/;
+    $app->plugin('message');
+    $app->plugin('data');
+    $app->plugin('i18n');
+    $app->plugin( 'user' => { id => $session->{'user_id'} } );
+    
+    #
+    #   Joker's routes.
+    #
+    
+    my $route = $app->routes;
+    
+    $route->namespace('Mojolicious::Plugin::Controller');
+    
+    my $r = $route->bridge('/joker')->to('users#check_access');
+    
+    # List of plugins, which I found in ./lib/Mojolicious/Plugin.
+    $r->route('/')->to('joker#list')->name('joker_list');
+    
+    # Plugin's CRUD
+    # =============
+    # To "create" plugin, you need move it in plugin's directory.
+    # To "delete" it, you need remove its file form plugin's directory,
+    # but it does not purge configuration.
+    
+    # Read. Show plugin's info.
+    $r->route('/:plugin')->via('get')->to('joker#read')->name('joker_read');
+    
+    # Update. Only config is changable in plugin's info.
+	$r->route('/:plugin')->via('post')->to('joker#update')->name('joker_update');
+    
+    # Also you can turn it on or off... after restart application.
+    $r->route('/toggle/:plugin')->to('joker#toggle')->name('joker_toggle');
+
+    #
+    #   Plugins.
+    #
+    
+    my @plugins = $app->data->read('plugins');
+    
+    # Change state.
+    map {
+        # Annul change-bit.
+        $_->{'state'} &= 0b011;
+        # Invert active-bit if invert-bit is 1.
+        $_->{'state'} ^= 0b011 if $_->{'state'} & 0b010;
+    } @plugins;
+    
+    $app->data->update(
+        plugins => { state => $_->{'state'} }, { id => $_->{'id'} }
+    ) for @plugins;
+    
+    # Load active plugins.
+    @plugins = grep { $_->{'state'} == 0b001 } @plugins;
+    
+    # Exclude preload modules.
+    my %set;
+    ++$set{ lc $_->{'id'} } for @plugins;
+    delete $set{ lc $_ } for @preload_modules;
+    @plugins = keys %set;
+    
+    $app->plugin( $_ ) for @plugins;
+    
+	#
+	#   Helpers.
+	#
+	
+    $app->helper (
+    	# Recursive build html tables for config structure.
+    	# FIXME: it's dirty.
+        html_hash_tree => sub {
+            my ( $self, $config, $parent ) = @_;
+            my $ret;
+            $parent ||= '';
+            
+            for my $k ( keys %$config ) {
+                $ret .= "<tr><td>$k</td><td name='$parent-$k'>";
+                
+                # Branch or leaf?
+                $ret .= ref $config->{$k} ?
+                    $self->html_hash_tree( $config->{$k}, "$parent-$k" ) :
+                    "<input value='" . $config->{$k} . "' name='$parent-$k-input'>";
+                
+                $ret .= "</td></tr>";
+            }
+
+            return "<table>$ret</table>";
+        }
+    );
+    
+    $app->helper(
+        # Render from __DATA__ section.
+        render_data_section => sub {
+            my ( $self, $package, $template ) = @_;
+            
+            unless ( defined $template ) {
+                $template = $package;
+                $package = __PACKAGE__;
+            }
+
+            my $DATA = Mojo::Command->new->get_all_data( __PACKAGE__ );
+            
+            my $BODY = $package ne __PACKAGE__ ? 
+                Mojo::Command->new->get_all_data( $package ) : $DATA;
+            
+            $self->content_for (
+                body => $self->render( inline => $BODY->{"$template.html.ep"} )
+            );
+            
+            $self->render( inline => $DATA->{'base.html.ep'} );
+        }
+    );
+}
+
+1;
+
 package Mojolicious::Plugin::Controller::Joker;
 use Storable qw/freeze thaw/;
 
@@ -38,7 +168,7 @@ sub read {
         title => $info->{'name'}
     );
 
-    $self->render_data_section('read');
+    $self->render;
 }
 
 #
@@ -66,7 +196,7 @@ sub list {
         title => 'list'
     );
 
-    $self->render_data_section('list');
+    $self->render;
 }
 
 sub update {
@@ -229,352 +359,6 @@ sub path2module {
 }
 
 1;
-
-package Mojolicious::Plugin::Joker;
-
-use base 'Mojolicious::Plugin';
-use Storable 'thaw';
-
-our $VERSION = '0.2';
-
-# Will run once.
-sub register {
-    my ( $self, $app ) = @_;
-    
-    my $session = $app->sessions;
-    
-    # Preload core plugins.
-    my @preload_modules = qw/message data user/;
-    $app->plugin('message');
-    $app->plugin('data');
-    $app->plugin( 'user' => { id => $session->{'user_id'} } );
-    
-    #
-    #   Joker's routes.
-    #
-    
-    my $r = $app->routes;
-    $r->namespace('Mojolicious::Plugin::Controller');
-    
-    # List of plugins, which I found in ./lib/Mojolicious/Plugin.
-    $r->route('/joker')->to('joker#list')->name('joker_list');
-    
-    # Plugin's CRUD
-    # =============
-    # To "create" plugin, you need move it in plugin's directory.
-    # To "delete" it, you need remove its file form plugin's directory,
-    # but it does not purge configuration.
-    
-    # Read. Show plugin's info.
-    $r->route('/joker/:plugin')->via('get')->to('joker#read')->name('joker_read');
-    
-    # Update. Only config is changable in plugin's info.
-	$r->route('/joker/:plugin')->via('post')->to('joker#update')->name('joker_update');
-    
-    # Also you can turn it on or off... after restart application.
-    $r->route('/joker/toggle/:plugin')->to('joker#toggle')->name('joker_toggle');
-
-    #
-    #   Plugins.
-    #
-    
-    my @plugins = $app->data->read('plugins');
-    
-    # Change state.
-    map {
-        # Annul change-bit.
-        $_->{'state'} &= 0b011;
-        # Invert active-bit if invert-bit is 1.
-        $_->{'state'} ^= 0b011 if $_->{'state'} & 0b010;
-    } @plugins;
-    
-    $app->data->update(
-        plugins => { state => $_->{'state'} }, { id => $_->{'id'} }
-    ) for @plugins;
-    
-    # Load active plugins.
-    @plugins = grep { $_->{'state'} == 0b001 } @plugins;
-    
-    # Exclude preload modules.
-    my %set;
-    ++$set{$_}      for @plugins;
-    delete $set{$_} for @preload_modules;
-    @plugins = keys %set;
-    
-    $app->plugin( lc $_->{'id'} ) for @plugins;
-    
-	#
-	#   Helpers.
-	#
-	
-    $app->helper (
-    	# Recursive build html tables for config structure.
-    	# FIXME: it's dirty.
-        html_hash_tree => sub {
-            my ( $self, $config, $parent ) = @_;
-            my $ret;
-            $parent ||= '';
-            
-            for my $k ( keys %$config ) {
-                $ret .= "<tr><td>$k</td><td name='$parent-$k'>";
-                
-                # Branch or leaf?
-                $ret .= ref $config->{$k} ?
-                    $self->html_hash_tree( $config->{$k}, "$parent-$k" ) :
-                    "<input value='" . $config->{$k} . "' name='$parent-$k-input'>";
-                
-                $ret .= "</td></tr>";
-            }
-
-            return "<table>$ret</table>";
-        }
-    );
-    
-    $app->helper(
-        # Render from __DATA__ section.
-        render_data_section => sub {
-            my ( $self, $package, $template ) = @_;
-            
-            unless ( defined $template ) {
-                $template = $package;
-                $package = __PACKAGE__;
-            }
-
-            my $DATA = Mojo::Command->new->get_all_data( __PACKAGE__ );
-            
-            my $BODY = $package ne __PACKAGE__ ? 
-                Mojo::Command->new->get_all_data( $package ) : $DATA;
-            
-            $self->content_for (
-                body => $self->render( inline => $BODY->{"$template.html.ep"} )
-            );
-            
-            $self->render( inline => $DATA->{'base.html.ep'} );
-        }
-    );
-}
-
-1;
-
-__DATA__
-@@ base.html.ep
-<!doctype html>
-<html>
-    <head>
-        <style>
-.page {
-	margin:0px auto;
-	width:600px;
-	color:#222;
-	font-family:Sans, Arial;
-}
-a {
-    color: #c62;
-}
-.rounded {
-	padding: 5px;
-	background: #eee;
-	border: 1px solid #ddd;
-	-moz-border-radius: 10px;
-	-webkit-border-radius: 10px;
-	border-radius: 10px;
-	-khtml-border-radius: 10px;
-}
-.info {
-    padding: 5px 5px 5px 15px;
-    margin: 0px 0px 10px 10px;
-	background: #ffd;
-	border: 1px solid #fff7dd;
-	-moz-border-radius: 4px;
-	-webkit-border-radius: 4px;
-	border-radius: 4px;
-	-khtml-border-radius: 4px;
-}
-.simple_table {
-    border:0px;
-    //width:600px;
-}
-.simple_table td {
-    vertical-align: top;
-    padding:2px 5px 2px 5px;
-}
-td.changeable {
-    border:1px solid #ddd;
-}
-.alignright {
-    float:right;
-}
-body {
-    padding:0px;
-    margin:0px;
-    font-family: Sans-Serif,'Lucida Grande';
-    font-size: 14px;
-}
-img {
-    border:0px;
-}
-.plugin {
-    margin-top:5px;
-    padding: 10px;
-}
-.actions {
-    float:right;
-}
-a.action:hover {
-    background-color:#ddd;
-}
-.action {
-    float:left;
-    display:inline;
-    padding:5px 10px 5px 10px;
-    margin:-6px 0px 0px 10px;
-}
-.plugins {
-    margin:0px auto;
-    width:600px;
-}
-.plugins h2 {
-    color:#ccc;
-}
-.on, a.on {
-    font-weight:bold;
-    color:#a00;
-    font-family:"Times new roman";
-    text-decoration: none;
-    display:inline;
-}
-.off, a.off {
-    font-weight:bold;
-    color:#ccc;
-    font-family:"Times new roman";
-    text-decoration: none;
-    display:inline;
-}
-        </style>
-        <link rel="icon" href="/img/joker/j64.png" type="image/x-icon" />
-		<link rel="shortcut icon" href="/img/joker/j64.png" type="image/x-icon" />
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>Joker plugin system &rarr; <%= $title %></title>
-    </head>
-    <body>
-        <div class=page>
-            %= content_for 'body'
--->     </div>
-    </body>
-</html>
-
-@@ list.html.ep
-% content_for body => begin
-<div class="plugins">
-<h2>Plugin list</h2>
-%   my $jokes = $self->stash('jokes');
-%   for my $plugin (values %$jokes) {
-%       my $active = $plugin->{'state'} & 0b001;
-%       my $invert = $plugin->{'state'} & 0b010;
-%       my $change = $plugin->{'state'} & 0b100;
-%
-        <div class="plugin rounded">
-            <a href="<%= url_for('joker_read', plugin => $plugin->{'name'}) %>">
-                <%= $plugin->{'name'} %></a> (<%= $plugin->{'version'} %>)
-            <div class="actions">
-% if ( $invert ) {
-                <div class="rounded action">
-                    <span class="<%= $active ? 'on' : 'off' %>">J</span> &rarr;
-                    <span class="<%= $active ? 'off' : 'on' %>">J</span>
-                </div>
-% }
-% if ( $change ) {
-                <div class="rounded on action">*</div> &nbsp;
-% }
-                <div class="action 
-%= $active ? 'on' : 'off';
-                    rounded" href="<%= url_for('joker_toggle', plugin => $plugin->{'name'}) %>">J</div>
-            </div>
-        </div>
-%   }
-</div>
-<!-- End of body 
-% end
-
-@@ read.html.ep
-% content_for body => begin
-% my $plugin = $self->stash('jokes');
-%
-% my $active = $plugin->{'state'} & 0b001;
-% my $invert = $plugin->{'state'} & 0b010;
-% my $change = $plugin->{'state'} & 0b100;
-%
-<div class="plugins">
-    <h2><a class="on rounded action" href="<%= url_for('joker_list') %>">J</a>&nbsp;&rarr;
-        <%= $plugin->{'name'} %></h2>
-%#
-%#          Show notifications
-%#
-% if ( $invert ) {
-    <div class="info">
-        Joke should be restarted to turn it <%= $active ? 'off' : 'on' %>.
-    </div>
-% }
-% if ( $change ) {
-    <div class="info">
-        Joke should be restarted to apply changes.
-    </div>
-% }
-    <div class="actions">
-        <div class="action <%= $active ? 'on' : 'off' %> rounded">J</div>
-    </div>
-%#
-%#          PLUGIN's INFO
-%#
-<div class="plugins rounded">
-    <table class="simple_table">
-        <tr>
-            <td>about</td>
-            <td><%= $plugin->{'about'} %>
-                <a href=<%= url_for('joker_toggle', plugin => $plugin->{'name'}) %>>
-                    Turn it <%= ($active xor $invert) ? 'off' : 'on' %>
-                </a>
-            </td>
-        </tr>
-        <tr>
-            <td>version</td>
-            <td><%= $plugin->{'version'} %></td>
-        </tr>
-        <tr>
-            <td>depends</td>
-            <td>
-%#
-%#          LIST OF DEPS
-%#
-% for my $dep ( @{$plugin->{'depends'}} ) {
-    <a href="<%= url_for('joker_read', plugin => $dep)%>"><%= $dep %></a>&nbsp;
-% }
-            </td>
-        </tr>
-        <tr>
-            <td>config</td>
-            <td>
-% if ( keys %{$plugin->{'config'}} ) {
-<form action="<%= url_for('joker_update', plugin => $plugin->{'name'}) %>"
-    method=POST>
-%== html_hash_tree( $plugin->{'config'} );
-    <input type="submit" class="alignright">
-</form>
-% }
-% else {
-    empty
-% }
-            </td>
-        </tr>
-        <tr>
-            <td>author</td>
-            <td><%= $plugin->{'author'} %></td>
-        </tr>
-    </table>
-</div>
-</div>
-<!-- End of body 
-% end
 
 __END__
 
--- lib/Mojolicious/Plugin/User.pm
+++ lib/Mojolicious/Plugin/User.pm
@@ -38,22 +38,43 @@ sub register {
     $app->secret( $self->info('config')->{'cookies'} );
     $app->sessions->default_expiration(3600*24*7);
     
-    my $session = $app->sessions;
-    my $id = $session->{'user_id'};
+    # Routes
+    my $route = $app->routes;
+    my $r = $route->bridge('/user')->to('users#check_access');
     
-    # Anonymous has 1st id.
-    $id ||= 1;
+    # CRUD for User.
+    $r->route('/new')->via('get')->to('users#create_form')->name('users_create_form');
+    $r->route('/new')->via('post')->to('users#create')->name('users_create');
+    $r->route('/:id', id => qr/\d+/)->via('post')->to('users#read')->name('users_read');
+    $r->route('/:id', id => qr/\d+/)->via('put')->to('users#update')->name('users_update');
+    $r->route('/:id', id => qr/\d+/)->via('delete')->to('users#delete')->name('users_delete');
+    # +List
+    $r->route('/list/:id', id => qr/\d+/)->via('get')->to('users#create')->name('users_list');
     
-    my $user = Mojolicious::Plugin::User::User->new (
-        $app->data->read( users => { id => $id } )
-    );
+    # Auth
+    $r->route('/login')->via('get')->to('users#login_form')->name('users_login_form');
+    $r->route('/login')->via('post')->to('users#login')->name('users_login');
+    $r->route('/logout')->to('users#logout')->name('users_logout');
     
-    # Cann't init user.
-    #$self->error("User with this id does not exist.") unless ref($user);
+    # Shared object.
+    my $obj;
     
     $app->helper (
         user => sub {
-            return $user;
+            my ( $self, $new_obj ) = @_;
+            
+            if ( defined $new_obj ) {
+                $obj = $new_obj;
+            }
+            
+            return $obj;
+        }
+    );
+    
+    $app->helper (
+        sess => sub {
+            my $session = $app->sessions;
+            return $session;
         }
     );
 }
@@ -63,9 +84,9 @@ sub register {
 package Mojolicious::Plugin::User::User;
 
 sub new {
-    my ($self, @users) = @_;
+    my ( $self, @users ) = @_;
     
-    return 0 if $#users;
+    return bless {}, $self if $#users;
 
     bless $users[0], $self;
 }
@@ -82,6 +103,142 @@ sub is_admin {
     # In soviet Russia^W^W phpbb3 it's true.
     return 1 if $self->{'role'} == 3;
     return 0;
+}
+
+1;
+
+package Mojolicious::Plugin::Controller::Users;
+
+use base 'Mojolicious::Controller';
+
+sub read {
+    my $self = shift;
+    
+	# Get accounts by id.
+	my @users = $self->select( users => '*', { id => $self->stash('id') } );
+    
+    $self->error("User with this id doesn't exist!") if $#users;
+    
+    if( $self->user->{'id'} != 1                        # not Anonymous
+        && $self->stash('id') == $self->user->{'id'}    # and Self
+        || $self->user->is_admin() ) {                  # or  Admin.
+        
+        $self->read_extended(@users);
+    }
+
+    $self->stash( user => $users[0] );
+
+    $self->render;
+}
+
+sub read_extended {
+    my $self = shift;
+    my @users = @_;
+    
+    $self->render(
+        action => 'read_extended',
+    );
+}
+
+sub create_form {
+    shift->render;
+}
+
+sub create {
+    my $self = shift;
+    
+    my $key = 
+    
+    #
+    #   Send mail
+    #
+    
+    $self->data->create( users => {
+        mail    => $self->param('mail'),
+        regdate => time,
+        confirm_time => time + 60*60*24*7,
+        
+    });
+}
+
+sub check_access {
+    my $self = shift;
+    
+    my $id = $self->sess->{'user_id'};
+    
+    # Anonymous has 1st id.
+    $id ||= 1;
+    
+    $self->user( new Mojolicious::Plugin::User::User (
+        $self->data->read( users => { id => $id } )
+    ) );
+    
+    #
+    #   TODO: some easy acl.
+    #
+    
+    return 1;
+    if ( $self->user->is_admin ) {
+        return 1;
+    }
+    
+    $self->error('You have not access for this page!');
+    return 0
+}
+
+sub login {
+    #
+    #   TODO: Login attempts counter for ip.
+    #
+    
+	my $self = shift;
+	
+	# It's not an e-mail!
+	$self->IS( mail => $self->param('mail')	);
+	
+	# Get accounts by e-mail.
+	my @users = $self->select( users => '*', {email => $self->param('mail')} );
+    
+    # If this e-mail does not exist
+    # or more than one account has this e-mail.
+    $self->error("This pair(e-mail and password) doesn't exist!") if $#users;
+    
+    my $user = $users[0];
+    
+    # Password test:
+    #   hash != md5( regdate + password + salt )
+    my $s = $user->{'regdate'} . $self->param('passwd') . $self->stash('salt');
+    
+    if ( $user->{'password'} ne Digest::MD5::md5_hex($s) ) {
+        $self->error( "This pair(e-mail and password) doesn't exist!" );
+        
+        # Don't work without return. I don't know why.
+        return;
+    }
+    
+    # Init session.
+    $self->session (
+        user_id  => $user->{'id'},
+    )->redirect_to( 'user_read', id => $user->{'id'} );
+}
+
+sub logout {
+    my $self = shift;
+    
+    # Delete session.
+	$self->session(
+		user_id  => '',
+	)->redirect_to('index');
+}
+
+sub login_form {
+    my $self = shift;
+    
+    #
+    #   TODO: Login counter and CAPTCHA
+    #
+    $self->stash( title => 'Login' );
+    $self->render_data_section(__PACKAGE__, 'login_form');
 }
 
 1;
