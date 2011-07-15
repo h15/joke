d0792d531fa00784851b3c6f9e83b66896124fbc
Georgy Bazhukov <helios_pro@mail.ru>
2011-07-15 23:41:09 +0600

    forgot commit base class

--- lib/Joke.pm
+++ lib/Joke.pm
@@ -1,9 +1,13 @@
 package Joke;
 use Mojo::Base 'Mojolicious';
+use Joke::Model;
+
+has model_obj => undef;
 
 # This method will run once at server start
 sub startup {
     our $self = shift;
+        $self->model_obj( new Joke::Model );
 }
 
 sub import {
@@ -21,6 +25,26 @@ sub import {
        
     push @{"${caller}::ISA"}, $base, 'Mojo::Base';
          *{"${caller}::app"} = sub { $Joke::self };
+}
+#"
+
+sub model {
+    my ( $self, $name, $id ) = @_;
+    
+    return $self->model_obj unless $name;
+    
+    # Defined NAME
+    $self->model_obj->model($name);
+    $self->model_obj->add_model( $name => 1 )
+        unless exists $self->model_obj->models->{$name};
+    
+    # Defined ID
+    if ( defined $id ) {
+        return $self->model_obj->find (
+            $self->model($name)->raw->meta->primary_key || 'id' => $id );
+    }
+    
+    return $self->model_obj;
 }
 
 1;
